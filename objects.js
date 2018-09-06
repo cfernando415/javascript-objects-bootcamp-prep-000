63ca65e84ff448fcd5797603f7ee1be7b3d57fd8
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-06 14:56:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({MattMaher: 'Lord I Need You'})
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist[artistName] = songTitle;
 }
 
 function removeFromPlaylist(playlist, artistName) {
